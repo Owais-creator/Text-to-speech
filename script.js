cebe465831b3a69c9f7e240b4dea723dd802cd26
owais-creator <taiowais717@gmail.com>
2026-01-21 18:43:53 +0500

    okieeeeeeeeeeeeee

---
+++ script.js
@@ -0,0 +1,44 @@
+let  voiceSelect = document.getElementById("voiceSelect");
+let  synth = window.speechSynthesis;
+
+let voices = [];
+
+let populateVoiceList = () => {
+    try {
+        voices = synth.getVoices();
+        voiceSelect.innerHTML = ''; 
+        voices.forEach((voice, index) => {
+            const option = document.createElement('option');
+            option.textContent = `${voice.name} (${voice.lang})`;
+            option.value = index;
+            voiceSelect.appendChild(option);
+        });
+    } catch (error) {
+        console.error("Error populating voice list:", error);
+    }
+};
+
+
+try {
+    populateVoiceList();
+    if (speechSynthesis.onvoiceschanged !== undefined) {
+        speechSynthesis.onvoiceschanged = populateVoiceList;
+    }
+} catch (error) {
+    console.error("Error initializing voice list or setting event listener:", error);
+}
+
+let speak = () => {
+    try {
+        
+        let text = document.getElementById("text").value;
+        let speech = new SpeechSynthesisUtterance(text);
+
+        const selectedVoice = voices[voiceSelect.value];
+        speech.voice = selectedVoice;
+
+        speechSynthesis.speak(speech);
+    } catch (error) {
+        console.error("Error speaking text:", error);
+    }
+}
